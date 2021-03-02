2fd3f8e1dd5402ad539301b5822340ea32e3a7be
RobKlock <rklock@oberlin.edu>
2021-03-02 17:14:33 -0500

    Working

--- neuronLatch.py
+++ neuronLatch.py
@@ -11,7 +11,6 @@ import numpy as np
 import math
 import sys
 sys.path.append('/Users/robertklock/Documents/Class/Fall20/SimenLab/simenlab')
-import perceptron as p
 import matplotlib.pyplot as plt
 
 def sigmoid(l, total_input, b):
@@ -27,11 +26,11 @@ data = np.zeros((1,300))
 data[0][20:30] = 1
 data[0][50:60] = 1
 
-weights = np.array([[1.7,  -1.7],     # 1->1, 2->1
-                    [1,  4]])   # 1->2, 2->2
+weights = np.array([[1.5,  -2],     # 1->1, 2->1
+                    [1.0,  1.75]])   # 1->2, 2->2
     
-beta = 1.1
-lmbd = 4
+beta = 1.2
+lmbd = 3.5
 v_hist = np.array([[0, 0]]).T    
 
 steps = 0 
@@ -51,7 +50,7 @@ for i in data[0]:
     activations = weights @ v # sum of activations, inputs to each unit
     activations[0] = i + activations[0]    
     activations[1] = sigmoid(lmbd, activations[1], bias[1])
-    dv = tau * ((-v + activations) * dt * np.sqrt(dt)) #* np.random.normal(0,1, (6,1)))  add noise using np.random
+    dv = (1/tau) * ((-v + activations) * dt * np.sqrt(dt))+ np.random.normal(.5,.1, (2,1))  #add noise using np.random
     #dv = (1/tau) * (((-v) + activations) * dt) + (np.sqrt(dt)) / tau) add noise using np.random
     
     v = v + dv
@@ -61,9 +60,11 @@ for i in data[0]:
 fig, axs = plt.subplots(3)
 fig.suptitle("Unit Activations and Stimulus")
 axs[0].plot(v_hist[1,:]) 
-axs[0].set_ylabel("Unit 2 Activation")
+axs[0].set_ylabel("OUT Unit Activation")
+axs[0].grid('on')
 axs[1].plot(v_hist[0,:])
-axs[1].set_ylabel("Unit 1 Activation")
+axs[1].set_ylabel("IN Unit Activation")
+axs[1].grid('on')
 axs[2].plot(data[0])
 axs[2].set_ylabel("Input Stimulus")
 plt.xlabel("Time")
@@ -72,7 +73,7 @@ plt.grid('on')
 x_axis_vals = np.arange(-2, 3, .1)
 
 plt.figure()
-plt.plot(x_axis_vals, graph_sigmoid(l[1], x_axis_vals, bias[1] - v[0]))
+plt.plot(x_axis_vals, graph_sigmoid(l[1], x_axis_vals, bias[1] - v[1]))
 x1 = [0, 3]
 y2 = [0, 1/weights[1,1] * 3]
 plt.plot(x1,y2, label = "strength of unit 2")

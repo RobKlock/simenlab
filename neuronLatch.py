20ea2657fb1ca7455ad2d0302b1cd31e14637b1e
RobKlock <rklock@oberlin.edu>
2021-03-02 19:24:46 -0500

    Working with fixed timesteps

--- neuronLatch.py
+++ neuronLatch.py
@@ -22,23 +22,23 @@ def graph_sigmoid(l, I, b):
     return f
 # Setup our time series data, which is a series of zeros with two batches of 1's from
 # 20-30 and 50-60
-data = np.zeros((1,300))
-data[0][20:30] = 1
-data[0][50:60] = 1
+dt = .01
+data = np.zeros((1,round(300/dt)))
+data[0][round(20/dt):round(30/dt)] = 1
+data[0][round(50/dt):round(60/dt)] = 1
 
-weights = np.array([[1.5,  -2],     # 1->1, 2->1
-                    [1.0,  1.75]])   # 1->2, 2->2
+weights = np.array([[2.0,  -1.0],     # 1->1, 2->1
+                    [1.0,  2.0]])   # 1->2, 2->2
     
-beta = 1.2
-lmbd = 3.5
+beta = 1.0
+lmbd = 4
 v_hist = np.array([[0, 0]]).T    
-
+noise = 0
 steps = 0 
 tau = 1
-dt = .1
 l = np.array([[lmbd, lmbd]]).T     
     
-bias = np.array([[beta, beta]]).T 
+bias = np.array([[1.2, beta]]).T 
 
 v_hist = np.array([[0, 0]]).T    
 v = np.array([[0, 0]]).T              
@@ -48,9 +48,10 @@ sig_idx= [0,1]
 for i in data[0]:
     
     activations = weights @ v # sum of activations, inputs to each unit
-    activations[0] = i + activations[0]    
+    activations[0] = sigmoid(lmbd, i + activations[0], bias[0])    
     activations[1] = sigmoid(lmbd, activations[1], bias[1])
-    dv = (1/tau) * ((-v + activations) * dt * np.sqrt(dt))+ np.random.normal(.5,.1, (2,1))  #add noise using np.random
+    # dv = (1/tau) * ((-v + activations) * dt) # No noise
+    dv = (1/tau) * ((-v + activations) * dt) + (noise * np.sqrt(dt) * np.random.normal(0, 1, (2,1)))  # Add noise using np.random
     #dv = (1/tau) * (((-v) + activations) * dt) + (np.sqrt(dt)) / tau) add noise using np.random
     
     v = v + dv
@@ -58,27 +59,30 @@ for i in data[0]:
     v_hist = np.concatenate((v_hist,v), axis=1)
 
 fig, axs = plt.subplots(3)
+activation_plot_xvals = np.arange(0, 300, dt)
 fig.suptitle("Unit Activations and Stimulus")
-axs[0].plot(v_hist[1,:]) 
+axs[0].plot(activation_plot_xvals, v_hist[1,0:-1]) 
 axs[0].set_ylabel("OUT Unit Activation")
+axs[0].set_ylim([0,1])
 axs[0].grid('on')
-axs[1].plot(v_hist[0,:])
+axs[1].plot(activation_plot_xvals, v_hist[0,0:-1])
 axs[1].set_ylabel("IN Unit Activation")
+axs[1].set_ylim([0,1])
 axs[1].grid('on')
-axs[2].plot(data[0])
+axs[2].plot(activation_plot_xvals, data[0])
 axs[2].set_ylabel("Input Stimulus")
-plt.xlabel("Time")
+axs[2].set_ylim([0,1])
+plt.xlabel("Time Steps")
 plt.grid('on')
 
-x_axis_vals = np.arange(-2, 3, .1)
-
+x_axis_vals = np.arange(-2, 3, dt)
 plt.figure()
-plt.plot(x_axis_vals, graph_sigmoid(l[1], x_axis_vals, bias[1] - v[1]))
+plt.plot(x_axis_vals, graph_sigmoid(l[1], x_axis_vals, bias[1] - v[0]))
 x1 = [0, 3]
 y2 = [0, 1/weights[1,1] * 3]
 plt.plot(x1,y2, label = "strength of unit 2")
 plt.ylim([0,1])
 plt.legend([ "sigmoid internal", "strength of unit 2"], loc = 0)
-plt.title("activation of 2 against sigmoid")
+plt.title("activation of OUT Unit against sigmoid")
 plt.grid('on')
 plt.show()

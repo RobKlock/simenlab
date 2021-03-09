7cab796bde1acefb4c6d09887cbead59c789d27c
RobKlock <rklock@oberlin.edu>
2021-03-09 18:00:27 -0500

    working

--- ideas.py
+++ ideas.py
@@ -27,26 +27,34 @@ def sq_error(t, o):
     # O is actual output
     return (t - o) ** 2
 
-# def state(unit_index, weights, )
+def state(unit_index, weights, v):
+    activations = weights[unit_index,:] @ v
+    return activations[0]
+
+def energy(weights, v):
+    return (-1/2) * v.T * weights * v
+    
 # Setup our time series data, which is a series of zeros with two batches of 1's from
 # 20-30 and 50-60
 dt = .01
-data = np.zeros((1,round(300/dt)))
-data[0][round(20/dt):round(30/dt)] = 1
-data[0][round(50/dt):round(60/dt)] = 1
+data1 = np.zeros((1,round(300/dt)))
+data1[0][round(20/dt):round(30/dt)] = 1
 
+
+data2 = np.zeros((1,round(300/dt)))
+data2[0][round(50/dt):round(60/dt)] = 3
 
 weights = np.array([[0,     0,      0,      0,      0,      0],       # 1->1, 2->1, 3->1, 4->1, 5->1, 6->1
                     [0,     0,      0,      0,      0,      0],       # 1->2, 2->2, 3->2, 4->2, 5->2, 6->2
-                    [1.0,   0,      2.0,    0,      -1,     0],       # 1->3, 2->3, 3->3, 4->3, 5->3, 6->3
-                    [-1.0,  1.0,    -1.0,   2.0,    0,      0],       # 1->4, 2->4, 3->4, 4->4, 5->4, 6->4
-                    [0,     -1.0,   0,      -1,     2.0,    0],       # 1->5, 2->5, 3->5, 4->5, 5->5, 6->5
+                    [2.0,   0,      2.0,    -1.0,   -1.0,     0],       # 1->3, 2->3, 3->3, 4->3, 5->3, 6->3
+                    [-2.0,  2.0,    -1.0,   2.0,    -1.0,      0],       # 1->4, 2->4, 3->4, 4->4, 5->4, 6->4
+                    [0,     -2.0,   -1.0,      -1,     2.0,    0],       # 1->5, 2->5, 3->5, 4->5, 5->5, 6->5
                     [0,     0,      1.0,    1.0,    1.0,    0]])      # 1->6, 2->6, 3->6, 4->6, 5->6, 6->6
     
 beta = 1.2 
 lmbd = 4
 v_hist = np.array([[0, 0,0,0,0,0]]).T    
-noise = 0
+noise = 0.03
 steps = 0 
 tau = 1
 l = np.array([[lmbd, lmbd,lmbd,lmbd,lmbd, lmbd]]).T     
@@ -55,22 +63,25 @@ bias = np.array([[1.2, 1.2,beta,beta,beta, beta]]).T
 
 v_hist = np.array([[0, 0,0,0,0,0]]).T    
 v = np.array([[0, 0,0,0,0,0]]).T              
-activations = [0.0,0.0,0.0,0.0,0.0,0.0]
+net_in = [0.0,0.0,0.0,0.0,0.0,0.0]
 sig_idx= [0,5]
 
-for i in data[0]:
+for i in range (0, data1.size):
     
-    activations = weights @ v # sum of activations, inputs to each unit
-    activations[0] = sigmoid(lmbd, i + activations[0], bias[0])    
-    activations[1] = sigmoid(lmbd, i + activations[1], bias[1])
+    net_in = weights @ v # sum of activations, inputs to each unit
+    net_in[0] = sigmoid(lmbd, data1[0][i], bias[0])    
+    net_in[1] = sigmoid(lmbd, data2[0][i], bias[1])
+    net_in[2:5] = sigmoid(l[2:5], net_in[2:5], bias[2:5])
     # dv = (1/tau) * ((-v + activations) * dt) # No noise
-    dv = (1/tau) * ((-v + activations) * dt) + (noise * np.sqrt(dt) * np.random.normal(0, 1, (6,1)))  # Add noise using np.random
+    dv = (1/tau) * ((-v + net_in) * dt) + (noise * np.sqrt(dt) * np.random.normal(0, 1, (6,1)))  # Add noise using np.random
     #dv = (1/tau) * (((-v) + activations) * dt) + (np.sqrt(dt)) / tau) add noise using np.random
     
     v = v + dv
     
     v_hist = np.concatenate((v_hist,v), axis=1)
 
+
+activation_plot_xvals = np.arange(0, 300, dt)
 plt.figure()
 activation_plot_xvals = np.arange(0, 300, dt)
 plt.plot(activation_plot_xvals, v_hist[0,0:-1], dashes = [2,2]) 
@@ -80,11 +91,14 @@ plt.plot(activation_plot_xvals, v_hist[3,0:-1], dashes = [3,3])
 plt.plot(activation_plot_xvals, v_hist[4,0:-1], dashes = [2,2])
 plt.ylim([0,1])
 #plt.plot(v2_v1_diff, dashes = [5,5])
-plt.legend(["v1","v2","v3","v4", "v5", "v6"], loc=0)
+plt.legend(["v1","v2","v3","v4", "v5"], loc=0)
 plt.ylabel("activation")
 plt.xlabel("steps")
 plt.grid('on')
 plt.show()
+
+plt.figure()
+plt.plot(activation_plot_xvals, v_hist[4,0:-1], dashes = [2,2])
     
 fig, axs = plt.subplots(3)
 activation_plot_xvals = np.arange(0, 300, dt)
@@ -97,7 +111,7 @@ axs[1].plot(activation_plot_xvals, v_hist[3,0:-1])
 axs[1].set_ylabel("IN Unit Activation")
 axs[1].set_ylim([0,1])
 axs[1].grid('on')
-axs[2].plot(activation_plot_xvals, data[0])
+axs[2].plot(activation_plot_xvals, data1[0])
 axs[2].set_ylabel("Input Stimulus")
 axs[2].set_ylim([0,1])
 plt.xlabel("Time Steps")

bbd5b11fef4ef25fa8fbf6f91731b86fb5761963
RobKlock <rklock@oberlin.edu>
2021-03-20 23:37:00 -0500

    learns weights when unit hits threshold too late in one shot

--- timing-model.py
+++ timing-model.py
@@ -55,8 +55,8 @@ def graph_pl(v, cutoff, bias):
 dt = .01
 data1 = np.zeros((1,round(300/dt)))
 data1[0][round(20/dt):round(40/dt)] = 1
-data1[0][round(70/dt):round(90/dt)] = 1
-data1[0][round(130/dt):round(135/dt)] = 1
+#data1[0][round(70/dt):round(90/dt)] = 1
+#data1[0][round(130/dt):round(135/dt)] = 1
 
 
 data2 = np.zeros((1,round(300/dt)))
@@ -71,7 +71,8 @@ weights = np.array([[2,   0,      0, 0],      # 1->1, 2->1, 3->1
 beta = 1.2
 third_unit_beta = 1.1
 lmbd = 4
-v_hist = np.array([[0, 0, 0, 0]]).T    
+v_hist = np.array([[0, 0, 0, 0]]).T 
+v_hist_test = np.array([[0, 0, 0, 0]]).T      
 noise = 0.0
 steps = 0 
 tau = 1
@@ -79,11 +80,12 @@ delta_A = 0
 l = np.array([[lmbd, lmbd, lmbd, lmbd]]).T     
 pl_slope = weights[1][1]
 bias = np.array([[beta, beta - 1.15, beta, beta]]).T 
-
-v_hist = np.array([[0, 0, 0, 0]]).T    
-v = np.array([[0.0, 0.0, 0.0, 0.0]]).T              
+ 
+v = np.array([[0.0, 0.0, 0.0, 0.0]]).T            
 net_in = [0.0,0.0, 0.0, 0.0]
+net_in_test = [0.0,0.0, 0.0, 0.0]
 timer_learn = False  
+
 for i in range (0, data1.size):
     
     net_in = weights @ v # sum of activations, inputs to each unit
@@ -110,7 +112,6 @@ for i in range (0, data1.size):
     #dv = (1/tau) * (((-v) + activations) * dt) + (np.sqrt(dt)) / tau) add noise using np.random
     v = v + dv
     v_hist = np.concatenate((v_hist,v), axis=1)
-
 
 activation_plot_xvals = np.arange(0, 300, dt)
 plt.figure()
@@ -182,5 +183,59 @@ axs[3].plot(activation_plot_xvals, data1[0])
 axs[3].set_ylabel("Input Stimulus")
 axs[3].set_ylim([0,1])
 plt.xlabel("Time Steps")
+plt.grid('on')
+plt.show()
+
+v_test = np.array([[0.0, 0.0, 0.0, 0.0]]).T     
+for i in range (0, data1.size):
+    
+    net_in_test = weights @ v_test # sum of activations, inputs to each unit
+    A = 1 / weights[1][0]
+    z = 1
+    # If the network gets a signal, start learning its duration
+    net_in_test[0] = sigmoid(l[0], data1[0][i] + net_in_test[0], bias[0])    
+    net_in_test[1] = piecewise_linear(net_in_test[1], pl_slope, bias[1])
+    net_in_test[2] = sigmoid(l[2], net_in_test[2], bias[2])
+    
+    # dv = (1/tau) * ((-v + activations) * dt) # No noise
+    dv = (1/tau) * ((-v_test + net_in_test) * dt) + (noise * np.sqrt(dt) * np.random.normal(0, 1, (4,1)))  # Add noise using np.random
+    #dv = (1/tau) * (((-v) + activations) * dt) + (np.sqrt(dt)) / tau) add noise using np.random
+    v_test = v_test + dv
+    v_hist_test = np.concatenate((v_hist_test,v_test), axis=1)
+
+fig, axs = plt.subplots(4)
+activation_plot_xvals = np.arange(0, 300, dt)
+fig.suptitle("Unit Activations and Stimulus")
+axs[0].plot(activation_plot_xvals, v_hist_test[2,0:-1]) 
+axs[0].set_ylabel("OUT Unit Activation")
+axs[0].set_ylim([0,1])
+axs[0].grid('on')
+axs[1].plot(activation_plot_xvals, v_hist_test[1,0:-1])
+axs[1].set_ylabel("MID Unit Activation")
+axs[1].set_ylim([0,1])
+axs[1].grid('on')
+axs[2].plot(activation_plot_xvals, v_hist_test[0,0:-1])
+axs[2].set_ylabel("IN Unit Activation")
+axs[2].set_ylim([0,1])
+axs[2].grid('on')
+axs[3].plot(activation_plot_xvals, data1[0])
+axs[3].set_ylabel("Input Stimulus")
+axs[3].set_ylim([0,1])
+plt.xlabel("Time Steps")
+plt.grid('on')
+plt.show()
+
+activation_plot_xvals = np.arange(0, 300, dt)
+plt.figure()
+activation_plot_xvals = np.arange(0, 300, dt)
+plt.plot(activation_plot_xvals, v_hist_test[0,0:-1], dashes = [2,2]) 
+plt.plot(activation_plot_xvals, v_hist_test[1,0:-1], dashes = [1,1])
+plt.plot(activation_plot_xvals, v_hist_test[2,0:-1], dashes = [1,1])
+plt.plot(activation_plot_xvals, v_hist_test[3,0:-1], dashes = [1,1])
+plt.ylim([0,1])
+#plt.plot(v2_v1_diff, dashes = [5,5])
+plt.legend(["v1_learned","v2_learned", "v3_learned", "v4_learned"], loc=0)
+plt.ylabel("activation")
+plt.xlabel("steps")
 plt.grid('on')
 plt.show()

857e01790dc667d33a619d2055b1ad660a431c54
RobKlock <rklock@oberlin.edu>
2021-03-27 18:05:19 -0400

    so confused. nothing is working

--- timing-model.py
+++ timing-model.py
@@ -34,18 +34,20 @@ def energy(weights, v):
     return (-1/2) * v.T * weights * v
 
 def piecewise_linear(v, cutoff, bias):
-    if ((v - bias) < 0):
+    if ((v - (bias/2)) < 0):
         return 0
+    elif ((v - (bias/2) >= 0) and (v - (bias/2) < cutoff)):
+        return (v - (bias/2)) / cutoff
     else:
-        return (v - bias) / cutoff
+        return 1
     
 def graph_pl(v, cutoff, bias):
     f = np.zeros(v.size)
     for i in range (0, v.size):
-        if (v[i] - bias < 0):
+        if (v[i] - (bias/2) < 0):
             f[i] = 0
-        elif (v[i] - bias >= 0) and (v[i] - bias < cutoff):
-            f[i] = ((v[i] - bias) / cutoff)
+        elif (v[i] - (bias/2) >= 0) and (v[i] - (bias/2) < cutoff):
+            f[i] = ((v[i] - (bias/2)) / cutoff)
         else:
             f[i] = 1
     return f
@@ -61,19 +63,20 @@ dt = .01
 data1 = np.zeros((1,round(300/dt)))
 data1[0][round(20/dt):round(40/dt)] = 1
 #data1[0][round(70/dt):round(90/dt)] = 1
-#data1[0][round(130/dt):round(135/dt)] = 1
+#data1[0][round(100/dt):round(120/dt)] = 1
 
 
 data2 = np.zeros((1,round(300/dt)))
 data2[0][round(50/dt):round(60/dt)] = 1
 
 weights = np.array([[2,   0,      0, 0],      # 1->1, 2->1, 3->1
-                    [.1,     2,    0, 0],      # 1->2, 2->2, 3->2
+                    [.08,     2,    0, 0],      # 1->2, 2->2, 3->2
                     [0,     0,      0.1, 0],      # 1->3, 2->3, 3->3
                     [0,     0,      0, 0]])      # 1->4, 2->4, 3->4
                          
     
 beta = 1.2
+ramp_bias = .1
 third_unit_beta = 1.1
 lmbd = 4
 v_hist = np.array([[0, 0, 0, 0]]).T 
@@ -84,35 +87,52 @@ tau = 1
 delta_A = 0
 l = np.array([[lmbd, lmbd, lmbd, lmbd]]).T     
 pl_slope = weights[1][1]
-bias = np.array([[beta, beta - 1.15, beta, beta]]).T 
+bias = np.array([[beta, ramp_bias, beta, beta]]).T 
  
 v = np.array([[0.0, 0.0, 0.0, 0.0]]).T            
 net_in = [0.0,0.0, 0.0, 0.0]
 net_in_test = [0.0,0.0, 0.0, 0.0]
 timer_learn = False  
-
+early = False
 for i in range (0, data1.size):
     
-    net_in = weights @ v # sum of activations, inputs to each unit
     A = 1 / weights[1][0]
-    z = 1
+    z = v[0]
     # If the network gets a signal, start learning its duration
     if data1[0][i] == 1:
         timer_learn = True 
     
-    if (data1[0][i] == 0) and (timer_learn == True):
-        # If we hit our target late 
-        if (round_up(v[1], 1)) <= .99:
-            # Do the late update
-            print("ok")
-            # Do the late update
-            x_end = net_in[1]
-            delta_A = A * ((z - x_end)/x_end)
-            timer_learn = False
-            weights[1][0] = weights[1][0] + delta_A
-            print(delta_A)
+#    if (data1[0][i] == 1) and (timer_learn == True) and (net_in[1] > z):
+#        # We hit our target early
+#        early = True
+#        print("Early")
+#        dA = (- ((weights[1][0] ** 2) / z)) * dt
+#        weights[1][0] = weights[1][0] + dA
+#        
+#    if (data1[0][i] == 0) and (timer_learn == True) and (early == True):
+#        # Done with the early correction
+#        timer_learn = False
+#        early = False
+    if (data1[0][i] == 0) and (timer_learn == True) and (not early):
+        # If we hit our target late
+    
+        # Do the late update
+      
+        Vt = v[1]
+        # The threshold on ramp-up values is C
+        # The input weight to the ramp is w
+        # The bias of the ramp unit is \beta
+        # Thus the drift imposed by the weight is \epsilon = w - \beta
+        # learning_rate * (W_end - beta) * (C - Cn) / Cn) <- Prof Simen's
+        
+        delta_A = (((weights[1][0]) - bias[1] + 0.5) * ((z - Vt)/Vt)) * dt
+        timer_learn = False
+        weights[1][0] = weights[1][0] + delta_A
+        print(delta_A)
             
+    net_in = weights @ v # sum of activations, inputs to each unit        
     net_in[0] = sigmoid(l[0], data1[0][i] + net_in[0], bias[0])    
+    #net_in[1] = sigmoid(l[1], net_in[0], bias[1])
     net_in[1] = piecewise_linear(net_in[1], pl_slope, bias[1])
     net_in[2] = sigmoid(l[2], net_in[2], bias[2])
     
@@ -120,9 +140,11 @@ for i in range (0, data1.size):
     # dv = (1/tau) * ((-v + activations) * dt) # No noise
     dv = (1/tau) * ((-v + net_in) * dt) + (noise * np.sqrt(dt) * np.random.normal(0, 1, (4,1)))  # Add noise using np.random
     #dv = (1/tau) * (((-v) + activations) * dt) + (np.sqrt(dt)) / tau) add noise using np.random
-    v = v + dv
-            
+    v = v + dv            
     v_hist = np.concatenate((v_hist,v), axis=1)
+    # If the stimulus stops and we're in learning mode and late...
+    
+        
 
 activation_plot_xvals = np.arange(0, 300, dt)
 plt.figure()
@@ -136,6 +158,7 @@ plt.ylim([0,1])
 plt.legend(["v1","v2", "v3", "v4"], loc=0)
 plt.ylabel("activation")
 plt.xlabel("steps")
+plt.title("Timer before learning")
 plt.grid('on')
 plt.show()
 
@@ -202,7 +225,7 @@ for i in range (0, data1.size):
     
     net_in_test = weights @ v_test # sum of activations, inputs to each unit
     A = 1 / weights[1][0]
-    z = 1
+    z = .999
     # If the network gets a signal, start learning its duration
     net_in_test[0] = sigmoid(l[0], data1[0][i] + net_in_test[0], bias[0])    
     net_in_test[1] = piecewise_linear(net_in_test[1], pl_slope, bias[1])
@@ -214,27 +237,6 @@ for i in range (0, data1.size):
     v_test = v_test + dv
     v_hist_test = np.concatenate((v_hist_test,v_test), axis=1)
 
-fig, axs = plt.subplots(4)
-activation_plot_xvals = np.arange(0, 300, dt)
-fig.suptitle("Unit Activations and Stimulus")
-axs[0].plot(activation_plot_xvals, v_hist_test[2,0:-1]) 
-axs[0].set_ylabel("OUT Unit Activation")
-axs[0].set_ylim([0,1])
-axs[0].grid('on')
-axs[1].plot(activation_plot_xvals, v_hist_test[1,0:-1])
-axs[1].set_ylabel("MID Unit Activation")
-axs[1].set_ylim([0,1])
-axs[1].grid('on')
-axs[2].plot(activation_plot_xvals, v_hist_test[0,0:-1])
-axs[2].set_ylabel("IN Unit Activation")
-axs[2].set_ylim([0,1])
-axs[2].grid('on')
-axs[3].plot(activation_plot_xvals, data1[0])
-axs[3].set_ylabel("Input Stimulus")
-axs[3].set_ylim([0,1])
-plt.xlabel("Time Steps")
-plt.grid('on')
-plt.show()
 
 activation_plot_xvals = np.arange(0, 300, dt)
 plt.figure()
@@ -249,4 +251,5 @@ plt.legend(["v1_learned","v2_learned", "v3_learned", "v4_learned"], loc=0)
 plt.ylabel("activation")
 plt.xlabel("steps")
 plt.grid('on')
+plt.title("Timer after learning")
 plt.show()
